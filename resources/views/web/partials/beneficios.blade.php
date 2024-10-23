7de535d35f58d48d11077a1bb7335cc8291fb482
Mersmith <mersmith14@gmail.com>
2024-10-23 13:54:56 -0500

    avance

--- resources/views/web/partials/beneficios.blade.php
+++ resources/views/web/partials/beneficios.blade.php
@@ -1,11 +1,20 @@
 @if (!empty($p_elemento) && !empty($p_elemento->contenido))
     <div class="partials_contenedor_celebracion">
+        <div class="titulo">
+            <h2>Beneficios de trabajo</h2>
+            <p>Descubre los beneficios por ser parte de nuestra familia.</p>
+        </div>
+
         <div class="swiper SwiperBeneficios-{{ $p_elemento->id }}">
             <!-- SLIDER -->
             <div class="swiper-wrapper">
                 @foreach ($p_elemento->contenido as $elemento)
                     <div class="swiper-slide">
-                        <p>{{ $elemento['titulo'] }}</p>
+                        <div class="cabecera">
+                            {!! $elemento['icono'] !!}
+                            <p>{{ $elemento['titulo'] }}</p>
+                        </div>
+                        <p>{{ $elemento['descripcion'] }}</p>
                     </div>
                 @endforeach
             </div>
@@ -17,9 +26,9 @@
 
     <script>
         var swiper = new Swiper('.SwiperBeneficios-{{ $p_elemento->id }}', {
-            slidesPerView: 5,  // Por defecto, se muestran 5 elementos
-            spaceBetween: 10,  // Espacio entre los elementos
-            loop: false,  // Carrusel en bucle
+            slidesPerView: 4, // Por defecto, se muestran 5 elementos
+            spaceBetween: 10, // Espacio entre los elementos
+            loop: false, // Carrusel en bucle
             navigation: {
                 nextEl: '.SwiperBeneficios-{{ $p_elemento->id }} .swiper-button-next',
                 prevEl: '.SwiperBeneficios-{{ $p_elemento->id }} .swiper-button-prev',
@@ -30,11 +39,11 @@
             },
             breakpoints: {
                 1000: {
-                    slidesPerView: 5,  // Mostrar 3 elementos en pantallas mayores a 1000px
+                    slidesPerView: 4, // Mostrar 3 elementos en pantallas mayores a 1000px
                     spaceBetween: 10,
                 },
                 500: {
-                    slidesPerView: 1,  // Mostrar 1 elemento en pantallas menores a 500px
+                    slidesPerView: 1, // Mostrar 1 elemento en pantallas menores a 500px
                     spaceBetween: 0,
                 }
             }
