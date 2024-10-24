11ef2bc56d9530670ad91b3021e2aa1cef41744a
Mersmith <mersmith14@gmail.com>
2024-10-24 14:38:52 -0500

    avance

--- resources/views/web/partials/planes-reserva.blade.php
+++ resources/views/web/partials/planes-reserva.blade.php
@@ -1,8 +1,23 @@
-<div class="partials_contenedor_planes">
-    @foreach ($p_elementos as $plan)
-        <div class="plan">
-            <h3>{!! $plan->icono !!} {{ $plan->titulo }}</h3>
+<div x-data="{ activePlan: 0 }" class="partials_contenedor_planes">
+    <!-- Botones de planes -->
+    <div class="botones_planes">
+        @foreach ($p_elementos as $index => $plan)
+            <button 
+                :class="{ 'active': activePlan === {{ $index }} }"
+                @click="activePlan = {{ $index }}"
+                class="boton_plan"
+            >
+                {!! $plan->icono !!} {{ $plan->titulo }}
+            </button>
+        @endforeach
+    </div>
 
+    <!-- Contenido de planes -->
+    @foreach ($p_elementos as $index => $plan)
+        <div 
+            :class="activePlan === {{ $index }} ? 'plan_contenido plan_contenido_activo' : 'plan_contenido'" 
+            class="plan_contenido"
+        >
             @if (isset($plan->reservas) && is_array($plan->reservas))
                 @foreach ($plan->reservas as $reserva)
                     <div class="reserva">
@@ -20,6 +35,5 @@
                 @endforeach
             @endif
         </div>
-        <br>
     @endforeach
 </div>
