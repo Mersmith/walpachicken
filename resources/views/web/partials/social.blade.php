6d3eb5fb27c06d1ea760da48b9a1e67f90d846ae
Mersmith <mersmith14@gmail.com>
2024-10-23 19:54:40 -0500

    avance

--- resources/views/web/partials/social.blade.php
+++ resources/views/web/partials/social.blade.php
@@ -1,80 +1,80 @@
 <div class="g_centrar_contenedor">
     <div class="partials_contenedor_social">
-        <div>
-            <h2>COMUNIDAD SOCIAL WALPA</h2>
+        <h2>COMUNIDAD SOCIAL WALPA</h2>
 
-            <div class="grid_social">
-                <div class="card_social social_facebook">
-                    <!-- TITULO SOCIAL -->
-                    <div class="titulo_social">
-                        <div class="logo">
-                            <img src="{{ asset('assets/images/logo.png') }}" alt="Tendencias Market"
-                                class="imagen_logo_computadora" />
-                        </div>
-                        <div class="info">
-                            <h4>@lalenaperu</h4>
-                            <h5>Restaurant</h5>
-                            <p>Si sabe a La Leña es nuestro</p>
-                        </div>
+        <div class="grid_social">
+            <div class="card_social social_facebook">
+                <!-- TITULO SOCIAL -->
+                <div class="titulo_social">
+                    <div class="logo">
+                        <img src="{{ asset('assets/images/logo.png') }}" alt="Tendencias Market"
+                            class="imagen_logo_computadora" />
                     </div>
-
-                    <div class="video">
-                        <video autoplay muted loop playsinline class="rounded_video">
-                            <source src="{{ asset('assets/videos/social.mp4') }}" type="video/mp4">
-                            Tu navegador no soporta videos HTML5.
-                        </video>
+                    <div class="info">
+                        <h4>@lalenaperu</h4>
+                        <h5>Restaurant</h5>
+                        <p>Si sabe a La Leña es nuestro</p>
                     </div>
-
-                    <div class="boton_seguir"><i class="fa-brands fa-facebook"></i> Seguir</div>
                 </div>
 
-                <div class="card_social social_instagram">
-                    <!-- TITULO SOCIAL -->
-                    <div class="titulo_social">
-                        <div class="logo">
-                            <img src="{{ asset('assets/images/logo.png') }}" alt="Tendencias Market"
-                                class="imagen_logo_computadora" />
-                        </div>
-                        <div class="info">
-                            <h4>@lalenaperu</h4>
-                            <h5>Restaurant</h5>
-                            <p>Si sabe a La Leña es nuestro</p>
-                        </div>
-                    </div>
-
-                    <div class="video">
-                        <video autoplay muted loop playsinline class="rounded_video">
-                            <source src="{{ asset('assets/videos/social.mp4') }}" type="video/mp4">
-                            Tu navegador no soporta videos HTML5.
-                        </video>
-                    </div>
-
-                    <div class="boton_seguir"><i class="fa-brands fa-instagram"></i> Seguir</div>
+                <!-- VIDEO -->
+                <div class="video">
+                    <video autoplay muted loop playsinline class="rounded_video">
+                        <source src="{{ asset('assets/videos/social.mp4') }}" type="video/mp4">
+                        Tu navegador no soporta videos HTML5.
+                    </video>
                 </div>
 
-                <div class="card_social social_tiktok">
-                    <!-- TITULO SOCIAL -->
-                    <div class="titulo_social">
-                        <div class="logo">
-                            <img src="{{ asset('assets/images/logo.png') }}" alt="Tendencias Market"
-                                class="imagen_logo_computadora" />
-                        </div>
-                        <div class="info">
-                            <h4>@lalenaperu</h4>
-                            <h5>Restaurant</h5>
-                            <p>Si sabe a La Leña es nuestro</p>
-                        </div>
-                    </div>
+                <!-- BOTON -->
+                <a class="boton_seguir"><i class="fa-brands fa-facebook"></i> Seguir</a>
+            </div>
 
-                    <div class="video">
-                        <video autoplay muted loop playsinline class="rounded_video">
-                            <source src="{{ asset('assets/videos/social.mp4') }}" type="video/mp4">
-                            Tu navegador no soporta videos HTML5.
-                        </video>
+            <div class="card_social social_instagram">
+                <!-- TITULO SOCIAL -->
+                <div class="titulo_social">
+                    <div class="logo">
+                        <img src="{{ asset('assets/images/logo.png') }}" alt="Tendencias Market"
+                            class="imagen_logo_computadora" />
                     </div>
-
-                    <div class="boton_seguir"><i class="fa-brands fa-tiktok"></i> Seguir</div>
+                    <div class="info">
+                        <h4>@lalenaperu</h4>
+                        <h5>Restaurant</h5>
+                        <p>Si sabe a La Leña es nuestro</p>
+                    </div>
                 </div>
+
+                <div class="video">
+                    <video autoplay muted loop playsinline class="rounded_video">
+                        <source src="{{ asset('assets/videos/social.mp4') }}" type="video/mp4">
+                        Tu navegador no soporta videos HTML5.
+                    </video>
+                </div>
+
+                <a class="boton_seguir"><i class="fa-brands fa-instagram"></i> Seguir</a>
+            </div>
+
+            <div class="card_social social_tiktok">
+                <!-- TITULO SOCIAL -->
+                <div class="titulo_social">
+                    <div class="logo">
+                        <img src="{{ asset('assets/images/logo.png') }}" alt="Tendencias Market"
+                            class="imagen_logo_computadora" />
+                    </div>
+                    <div class="info">
+                        <h4>@lalenaperu</h4>
+                        <h5>Restaurant</h5>
+                        <p>Si sabe a La Leña es nuestro</p>
+                    </div>
+                </div>
+
+                <div class="video">
+                    <video autoplay muted loop playsinline class="rounded_video">
+                        <source src="{{ asset('assets/videos/social.mp4') }}" type="video/mp4">
+                        Tu navegador no soporta videos HTML5.
+                    </video>
+                </div>
+
+                <a class="boton_seguir"><i class="fa-brands fa-tiktok"></i> Seguir</a>
             </div>
         </div>
     </div>
